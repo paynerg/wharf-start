cd4ed3696556ba1c6adad2a43a6a09b76b9be6f3
paynerg <payne.wg@gmail.com>
2016-11-20 23:43:24 -0500

    Added all the code from 2nd version of Wharf

--- app/src/main/java/com/findawharf/wharfagain/MainActivity.java
+++ app/src/main/java/com/findawharf/wharfagain/MainActivity.java
@@ -1,13 +1,69 @@
-package com.findawharf.wharfagain;
+package com.findawharf.newwharf;
 
+import android.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.findawharf.wharfagain.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String [] mMenuItems;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMenuItems = getResources().getStringArray(R.array.menu);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        //Adapter for input of string into Listview
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.id.drawer_list_item, mMenuItems));
+        // Set the list's click listener
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+    }
+
+    public class DrawerItemClickListener implements ListView.OnItemClickListener {
+
+        @Override
+        public void onItemClick (AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    //swaps fragment
+    private void selectItem(int position) {
+        //Create a new fragment and specify the fragment to show based on position
+        Fragment fragment = new MapFragment();
+        Bundle args = new Bundle();
+        args.putInt(MapFragment.ARG_MENU_NUMBER, position);
+        fragment.setArguments(args);
+
+        //Insert a fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.content_frame, fragment)
+                .commit();
+
+
+        // Highlight the selected item, update the title, and close the drawer
+        mDrawerList.setItemChecked(position, true);
+        setTitle(mMenuItems[position]);
+        mDrawerLayout.closeDrawer(mDrawerList);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        mTitle = title;
+        getActionBar().setTitle(mTitle);
     }
 }
